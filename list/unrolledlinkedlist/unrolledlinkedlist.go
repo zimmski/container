7ce0e6e93a24e75f3f7f0a1b7ff56f568d273a71
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-01-30 14:38:55 +0100

    start of unrolled linked list

---
+++ list/unrolledlinkedlist/unrolledlinkedlist.go
@@ -0,0 +1,511 @@
+package unrolledlinkedlist
+
+type Node struct {
+	next     *Node               // The node after this node in the list
+	previous *Node               // The node before this node in the list
+	list     *UnrolledLinkedList // The list to which this node belongs
+	values   []interface{}       // The values stored with this node
+}
+
+// Next returns the next node or nil
+func (n *Node) Next() *Node {
+	if i := n.next; n.list != nil {
+		return i
+	}
+
+	return nil
+}
+
+// Previous returns the previous node or nil
+func (n *Node) Previous() *Node {
+	if i := n.previous; n.list != nil {
+		return i
+	}
+
+	return nil
+}
+
+type Iterator struct {
+	current *Node // The current node in traversal
+	i       int   // The current index of the current node
+}
+
+func newIterator(current *Node, i int) *Iterator {
+	iter := new(Iterator)
+
+	iter.i = i
+	iter.current = current
+
+	return iter
+}
+
+func (iter *Iterator) Next() bool {
+	iter.i++
+
+	if iter.current != nil && iter.i >= len(iter.current.values) {
+		iter.i = 0
+		iter.current = iter.current.Next()
+	}
+
+	return iter.current != nil
+}
+
+func (iter *Iterator) Value() interface{} {
+	if iter.current == nil || iter.i < 0 || iter.i >= len(iter.current.values) {
+		return nil
+	}
+
+	return iter.current.values[iter.i]
+}
+
+type UnrolledLinkedList struct {
+	first       *Node // The first node of the list
+	last        *Node // The last node of the list
+	maxElements int   // Maximum of elements per node
+	len         int   // The current list length
+}
+
+// New returns an initialized list
+func New(maxElements int) *UnrolledLinkedList {
+	l := new(UnrolledLinkedList)
+
+	l.Clear()
+
+	l.maxElements = maxElements
+
+	return l
+}
+
+// Clear removes all nodes from the list
+func (l *UnrolledLinkedList) Clear() {
+	// TODO remove old values if they are there
+
+	l.first = nil
+	l.last = nil
+	l.len = 0
+}
+
+// Len returns the curren list length
+func (l *UnrolledLinkedList) Len() int {
+	return l.len
+}
+
+// newNode initializes a new node for the list
+func (l *UnrolledLinkedList) newNode(v interface{}) *Node {
+	return &Node{
+		list:   l,
+		values: make([]interface{}, 0, l.maxElements),
+	}
+}
+
+// insertNodeAfter creates a new node, inserts it after a given node and returns the new one
+func (l *UnrolledLinkedList) insertNodeAfter(p *Node) *Node {
+	if (p == nil && l.len != 0) || (p != nil && p.list != l) {
+		return nil
+	}
+
+	n := l.newNode(v)
+
+	// insert first node
+	if p == nil {
+		l.first = n
+		l.last = n
+	} else {
+		n.next = p.next
+		if p.next != nil {
+			p.next.previous = n
+		}
+		p.next = n
+		n.previous = p
+
+		if p == l.last {
+			l.last = n
+		}
+	}
+
+	return n
+}
+
+// First returns the first node of the list or nil
+func (l *UnrolledLinkedList) First() *Iterator {
+	if l.len == 0 {
+		return nil
+	}
+
+	return newIterator(l.first, 0)
+}
+
+// Last returns the last node of the list or nil
+func (l *UnrolledLinkedList) Last() *Iterator {
+	if l.len == 0 {
+		return nil
+	}
+
+	return newIterator(l.last, len(l.last.values)-1)
+}
+
+/*
+
+// Get returns the node with the given index or nil
+func (l *DoublyLinkedList) Get(i int) (*Node, error) {
+	if i < 0 || i >= l.len {
+		return nil, errors.New("index bounds out of range")
+	}
+
+	j := 0
+
+	for n := l.First(); n != nil; n = n.Next() {
+		if i == j {
+			return n, nil
+		}
+
+		j++
+	}
+
+	panic("there is something wrong with the internal structure")
+}
+
+// GetFunc returns the first node selected by a given function
+func (l *DoublyLinkedList) GetFunc(m func(n *Node) bool) *Node {
+	for n := l.First(); n != nil; n = n.Next() {
+		if m(n) {
+			return n
+		}
+	}
+
+	return nil
+}
+
+// Set replaces the value in the list with the given value
+func (l *DoublyLinkedList) Set(i int, v interface{}) error {
+	if i < 0 || i >= l.len {
+		return errors.New("index bounds out of range")
+	}
+
+	j := 0
+
+	for n := l.First(); n != nil; n = n.Next() {
+		if i == j {
+			n.Value = v
+
+			return nil
+		}
+
+		j++
+	}
+
+	panic("there is something wrong with the internal structure")
+}
+
+// SetFunc replaces the value of the first node selected by a given function
+func (l *DoublyLinkedList) SetFunc(m func(n *Node) bool, v interface{}) {
+	for n := l.First(); n != nil; n = n.Next() {
+		if m(n) {
+			n.Value = v
+
+			return
+		}
+	}
+}
+
+// Copy returns an exact copy of the list
+func (l *DoublyLinkedList) Copy() *DoublyLinkedList {
+	n := New()
+
+	for i := l.First(); i != nil; i = i.Next() {
+		n.Push(i.Value)
+	}
+
+	return n
+}
+
+// ToArray returns a copy of the list as slice
+func (l *DoublyLinkedList) ToArray() []interface{} {
+	a := make([]interface{}, l.len)
+
+	j := 0
+
+	for i := l.First(); i != nil; i = i.Next() {
+		a[j] = i.Value
+
+		j++
+	}
+
+	return a
+}
+
+// newNode initializes a new node for the list
+func (l *DoublyLinkedList) newNode(v interface{}) *Node {
+	return &Node{
+		list:  l,
+		Value: v,
+	}
+}
+
+// InsertAfter creates a new node from a value, inserts it after a given node and returns the new one
+func (l *DoublyLinkedList) InsertAfter(v interface{}, p *Node) *Node {
+	if (p == nil && l.len != 0) || (p != nil && p.list != l) {
+		return nil
+	}
+
+	n := l.newNode(v)
+
+	// insert first node
+	if p == nil {
+		l.first = n
+		l.last = n
+	} else {
+		n.next = p.next
+		if p.next != nil {
+			p.next.previous = n
+		}
+		p.next = n
+		n.previous = p
+
+		if p == l.last {
+			l.last = n
+		}
+	}
+
+	l.len++
+
+	return n
+}
+
+// InsertBefore creates a new node from a value, inserts it before a given node and returns the new one
+func (l *DoublyLinkedList) InsertBefore(v interface{}, p *Node) *Node {
+	if (p == nil && l.len != 0) || (p != nil && p.list != l) {
+		return nil
+	}
+
+	n := l.newNode(v)
+
+	// insert first node
+	if p == nil {
+		l.first = n
+		l.last = n
+	} else {
+		if p == l.first {
+			l.first = n
+		} else {
+			if p.previous != nil {
+				p.previous.next = n
+				n.previous = p.previous
+			}
+		}
+
+		n.next = p
+		p.previous = n
+	}
+
+	l.len++
+
+	return n
+}
+
+// InsertAt creates a new mnode from a value, inserts it at the exact index which must be in range of the list and returns the new node
+func (l *DoublyLinkedList) InsertAt(i int, v interface{}) (*Node, error) {
+	if i < 0 || i > l.len {
+		return nil, errors.New("index bounds out of range")
+	}
+
+	if i == 0 {
+		return l.Unshift(v), nil
+	} else if i == l.len {
+		return l.Push(v), nil
+	}
+
+	p, _ := l.Get(i)
+
+	return l.InsertBefore(v, p), nil
+}
+
+// Remove removes a given node from the list
+func (l *DoublyLinkedList) Remove(c *Node) *Node {
+	if c == nil || c.list != l || l.len == 0 {
+		return nil
+	}
+
+	if c == l.first {
+		l.first = c.next
+		if c.next != nil {
+			c.next.previous = nil
+		}
+
+		// c is the last node
+		if c == l.last {
+			l.last = nil
+		}
+	} else {
+		if c.previous != nil {
+			c.previous.next = c.next
+
+			if c.next != nil {
+				c.next.previous = c.previous
+			} else if c == l.last {
+				l.last = c.previous
+			}
+		}
+	}
+
+	c.list = nil
+	c.next = nil
+	c.previous = nil
+
+	l.len--
+
+	return c
+}
+
+// RemoveAt removes a node from the list at the given index
+func (l *DoublyLinkedList) RemoveAt(i int) (*Node, error) {
+	if i < 0 || i >= l.len {
+		return nil, errors.New("index bounds out of range")
+	}
+
+	c, _ := l.Get(i)
+
+	return l.Remove(c), nil
+}
+
+// RemoveFirstOccurrence removes the first node with the given value from the list and returns it or nil
+func (l *DoublyLinkedList) RemoveFirstOccurrence(v interface{}) *Node {
+	for i := l.First(); i != nil; i = i.Next() {
+		if i.Value == v {
+			return l.Remove(i)
+		}
+	}
+
+	return nil
+}
+
+// RemoveLastOccurrence removes the last node with the given value from the list and returns it or nil
+func (l *DoublyLinkedList) RemoveLastOccurrence(v interface{}) *Node {
+	for i := l.Last(); i != nil; i = i.Previous() {
+		if i.Value == v {
+			return l.Remove(i)
+		}
+	}
+
+	return nil
+}
+
+// Pop removes and returns the last node or nil
+func (l *DoublyLinkedList) Pop() *Node {
+	return l.Remove(l.last)
+}
+
+*/
+
+// Push creates a new node from a value, inserts it as the last node and returns it
+func (l *UnrolledLinkedList) Push(v interface{}) {
+	if l.last == nil || len(l.last.values) == cap(l.last.values) {
+		l.insertNodeAfter(l.last)
+	}
+
+	l.last.values = append(l.last.values, v)
+
+	l.len++
+}
+
+// PushList adds the values of a list to the end of the list
+func (l *UnrolledLinkedList) PushList(l2 *UnrolledLinkedList) {
+	iter := l.First()
+
+	if iter == nil {
+		return
+	}
+
+	for {
+		l.Push(iter.Value())
+
+		if !iter.Next() {
+			break
+		}
+	}
+}
+
+/*
+
+// Shift removes and returns the first node or nil
+func (l *DoublyLinkedList) Shift() *Node {
+	return l.Remove(l.first)
+}
+
+// Unshift creates a new node from a value, inserts it as the first node and returns it
+func (l *DoublyLinkedList) Unshift(v interface{}) *Node {
+	return l.InsertBefore(v, l.first)
+}
+
+// UnshiftList adds the values of a list to the front of the list
+func (l *DoublyLinkedList) UnshiftList(l2 *DoublyLinkedList) {
+	for i := l2.First(); i != nil; i = i.Next() {
+		l.Unshift(i.Value)
+	}
+}
+
+// Contains returns true if the value exists in the list
+func (l *DoublyLinkedList) Contains(v interface{}) bool {
+	_, ok := l.IndexOf(v)
+
+	return ok
+}
+
+// IndexOf returns the first index of an occurence of the given value and true or -1 and false if the value does not exist
+func (l *DoublyLinkedList) IndexOf(v interface{}) (int, bool) {
+	i := 0
+
+	for n := l.First(); n != nil; n = n.Next() {
+		if n.Value == v {
+			return i, true
+		}
+
+		i++
+	}
+
+	return -1, false
+}
+
+// LastIndexOf returns the last index of an occurence of the given value and true or -1 and false if the value does not exist
+func (l *DoublyLinkedList) LastIndexOf(v interface{}) (int, bool) {
+	i := l.len - 1
+
+	for n := l.Last(); n != nil; n = n.Previous() {
+		if n.Value == v {
+			return i, true
+		}
+
+		i--
+	}
+
+	return -1, false
+}
+
+// MoveAfter moves node n after node p
+func (l *DoublyLinkedList) MoveAfter(n, p *Node) {
+	if n.list != l || p.list != l || n == p {
+		return
+	}
+
+	l.InsertAfter(l.Remove(n).Value, p)
+}
+
+// MoveBefore moves node n before node p
+func (l *DoublyLinkedList) MoveBefore(n, p *Node) {
+	if n.list != l || p.list != l || n == p {
+		return
+	}
+
+	l.InsertBefore(l.Remove(n).Value, p)
+}
+
+// MoveToBack moves the given node after the last node of the list
+func (l *DoublyLinkedList) MoveToBack(n *Node) {
+	l.MoveAfter(n, l.last)
+}
+
+// MoveToFront moves the given node before the first node of the list
+func (l *DoublyLinkedList) MoveToFront(n *Node) {
+	l.MoveBefore(n, l.first)
+}
+*/
---
+++ list/unrolledlinkedlist/unrolledlinkedlist_test.go
@@ -0,0 +1,603 @@
+package unrolledlinkedlist
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+)
+
+var v = []interface{}{1, "a", 2, "b", 3, "c", 4, "d"}
+var vLen = len(v)
+
+func fillList(t *testing.T, l *UnrolledLinkedList) {
+	for i, va := range v {
+		l.Push(va)
+
+		Equal(t, l.Len(), i+1)
+		Equal(t, l.First().Value(), v[0])
+		Equal(t, l.Last().Value(), va)
+	}
+
+	Equal(t, l.Len(), vLen)
+}
+
+func newFilledList(t *testing.T) *UnrolledLinkedList {
+	l := New(4)
+
+	fillList(t, l)
+
+	return l
+}
+
+func TestBasic(t *testing.T) {
+	l := New(4)
+
+	Equal(t, l.Len(), 0)
+	Nil(t, l.First())
+	Nil(t, l.Last())
+	/*Nil(t, l.Pop())
+	Nil(t, l.Shift())*/
+
+	fillList(t, l)
+
+	i := 0
+	n := l.First()
+
+	for i < vLen {
+		Equal(t, v[i], n.Value())
+
+		i++
+		if i < vLen {
+			True(t, n.Next())
+		}
+	}
+
+	False(t, n.Next())
+	Nil(t, n.Value())
+
+	/*i = vLen - 1
+	n = l.Pop()
+
+	for i > -1 && n != nil {
+		Equal(t, v[i], n.Value)
+		Nil(t, n.Next())
+		Equal(t, l.Len(), i)
+
+		i--
+		n = l.Pop()
+	}
+
+	Equal(t, i, -1)
+	Nil(t, n)
+	Equal(t, l.Len(), 0)
+
+	for i, va := range v {
+		l.Unshift(va)
+
+		Equal(t, l.Len(), i+1)
+		Equal(t, l.First().Value, va)
+		Equal(t, l.Last().Value, v[0])
+	}
+
+	Equal(t, l.Len(), vLen)
+
+	i = vLen - 1
+	n = l.Shift()
+
+	for i > -1 && n != nil {
+		Equal(t, v[i], n.Value)
+		Nil(t, n.Next())
+		Equal(t, l.Len(), i)
+
+		i--
+		n = l.Shift()
+	}
+
+	Equal(t, i, -1)
+	Nil(t, n)
+	Equal(t, l.Len(), 0)*/
+}
+
+/*
+
+func TestToArray(t *testing.T) {
+	l := New()
+	Equal(t, l.ToArray(), []interface{}{})
+
+	fillList(t, l)
+	Equal(t, l.ToArray(), v)
+
+	l.Shift()
+	Equal(t, l.ToArray(), v[1:])
+
+	l.Pop()
+	Equal(t, l.ToArray(), v[1:3])
+}
+
+func TestInserts(t *testing.T) {
+	l := newFilledList(t)
+
+	i := 2
+	va := 1993
+
+	n, _ := l.Get(i)
+	Equal(t, n.Value, v[i])
+
+	l.InsertAfter(va, n)
+
+	Equal(t, l.Len(), vLen+1)
+	n, _ = l.Get(i)
+	Equal(t, n.Value, v[i])
+	r, _ := l.Get(i + 1)
+	Equal(t, r.Value, va)
+
+	l.InsertBefore(va, n)
+
+	Equal(t, l.Len(), vLen+2)
+	n, _ = l.Get(i + 1)
+	Equal(t, n.Value, v[i])
+	r, _ = l.Get(i)
+	Equal(t, r.Value, va)
+
+	// do not allow inserting with nodes of other lists
+	l2 := newFilledList(t)
+
+	Nil(t, l2.InsertAfter(v, n))
+	Nil(t, l2.InsertBefore(v, n))
+
+	// do not allow inserting before and after with nil node
+	Nil(t, l.InsertAfter(v, nil))
+	Nil(t, l.InsertBefore(v, nil))
+
+	// InsertAt
+	l1 := newFilledList(t)
+
+	l1.InsertAt(0, 0)
+	Equal(t, l1.ToArray(), []interface{}{0, 1, "a", 2, "b"})
+	Equal(t, l1.Len(), vLen+1)
+
+	l1.InsertAt(l1.Len(), 0)
+	Equal(t, l1.ToArray(), []interface{}{0, 1, "a", 2, "b", 0})
+	Equal(t, l1.Len(), vLen+2)
+
+	l1.InsertAt(2, 0)
+	Equal(t, l1.ToArray(), []interface{}{0, 1, 0, "a", 2, "b", 0})
+	Equal(t, l1.Len(), vLen+3)
+
+	// out of bound
+	_, err := l1.InsertAt(-1, 0)
+	NotNil(t, err)
+	_, err = l1.InsertAt(l1.Len()+1, 0)
+	NotNil(t, err)
+}
+
+func TestRemove(t *testing.T) {
+	l1 := newFilledList(t)
+	l2 := newFilledList(t)
+
+	// do not allow removing nodes from another list
+	n1, _ := l1.Get(1)
+	n2, _ := l2.Get(2)
+
+	Nil(t, l1.Remove(n2))
+	Equal(t, l1.Len(), vLen)
+	Nil(t, l2.Remove(n1))
+	Equal(t, l2.Len(), vLen)
+
+	Equal(t, n1.Value, l1.Remove(n1).Value)
+	Equal(t, l1.Len(), vLen-1)
+	Equal(t, n2.Value, l2.Remove(n2).Value)
+	Equal(t, l2.Len(), vLen-1)
+
+	l1 = newFilledList(t)
+
+	// out of bound
+	_, err := l1.RemoveAt(-1)
+	NotNil(t, err)
+	_, err = l1.RemoveAt(l1.Len())
+	NotNil(t, err)
+
+	// Remove Middle
+	n1, err = l1.RemoveAt(1)
+	Nil(t, err)
+	Equal(t, n1.Value, v[1])
+	Nil(t, n1.Next())
+	Nil(t, n1.Previous())
+	n2, _ = l1.Get(1)
+	Equal(t, n2.Value, v[2])
+	Equal(t, n2.Next().Value, v[3])
+	Equal(t, l1.Len(), 3)
+
+	// Remove First
+	n1, err = l1.RemoveAt(0)
+	Nil(t, err)
+	Equal(t, n1.Value, v[0])
+	Nil(t, n1.Next())
+	n2 = l1.First()
+	Equal(t, n2.Value, v[2])
+	Equal(t, n2.Next().Value, v[3])
+	Equal(t, l1.Len(), 2)
+
+	// Remove Last
+	n1, err = l1.RemoveAt(1)
+	Nil(t, err)
+	Equal(t, n1.Value, v[3])
+	Nil(t, n1.Next())
+	n2 = l1.Last()
+	Equal(t, n2.Value, v[2])
+	Nil(t, n2.Next())
+	Equal(t, l1.Len(), 1)
+
+	// Remove very last node
+	n1, err = l1.RemoveAt(0)
+	Nil(t, err)
+	Equal(t, n1.Value, v[2])
+	Nil(t, n1.Next())
+	Equal(t, l1.Len(), 0)
+	Nil(t, l1.First())
+	Nil(t, l1.Last())
+}
+
+func TestRemoveOccurrence(t *testing.T) {
+	l := New()
+
+	for i := 0; i < 5; i++ {
+		l.Push(i % 2)
+	}
+
+	n := l.RemoveFirstOccurrence(0)
+	Equal(t, n.Value, 0)
+	Nil(t, n.Next())
+	Equal(t, l.Len(), 4)
+	Equal(t, l.First().Value, 1)
+	Equal(t, l.First().Next().Value, 0)
+	Equal(t, l.First().Next().Next().Value, 1)
+	Equal(t, l.Last().Value, 0)
+
+	n = l.RemoveFirstOccurrence(0)
+	Equal(t, n.Value, 0)
+	Nil(t, n.Next())
+	Equal(t, l.Len(), 3)
+	Equal(t, l.First().Value, 1)
+	Equal(t, l.First().Next().Value, 1)
+	Equal(t, l.Last().Value, 0)
+
+	n = l.RemoveFirstOccurrence(0)
+	Equal(t, n.Value, 0)
+	Nil(t, n.Next())
+	Equal(t, l.Len(), 2)
+	Equal(t, l.First().Value, 1)
+	Equal(t, l.Last().Value, 1)
+
+	n = l.RemoveFirstOccurrence(0)
+	Nil(t, n)
+	Equal(t, l.Len(), 2)
+	Equal(t, l.First().Value, 1)
+	Equal(t, l.Last().Value, 1)
+
+	l.Clear()
+
+	for i := 0; i < 5; i++ {
+		l.Push(i % 2)
+	}
+
+	n = l.RemoveLastOccurrence(0)
+	Equal(t, n.Value, 0)
+	Nil(t, n.Next())
+	Equal(t, l.Len(), 4)
+	Equal(t, l.First().Value, 0)
+	Equal(t, l.First().Next().Value, 1)
+	Equal(t, l.First().Next().Next().Value, 0)
+	Equal(t, l.Last().Value, 1)
+
+	n = l.RemoveLastOccurrence(0)
+	Equal(t, n.Value, 0)
+	Nil(t, n.Next())
+	Equal(t, l.Len(), 3)
+	Equal(t, l.First().Value, 0)
+	Equal(t, l.First().Next().Value, 1)
+	Equal(t, l.Last().Value, 1)
+
+	n = l.RemoveLastOccurrence(0)
+	Equal(t, n.Value, 0)
+	Nil(t, n.Next())
+	Equal(t, l.Len(), 2)
+	Equal(t, l.First().Value, 1)
+	Equal(t, l.Last().Value, 1)
+
+	n = l.RemoveLastOccurrence(0)
+	Nil(t, n)
+	Equal(t, l.Len(), 2)
+	Equal(t, l.First().Value, 1)
+	Equal(t, l.Last().Value, 1)
+}
+
+func TestClear(t *testing.T) {
+	l := newFilledList(t)
+
+	l.Clear()
+
+	Equal(t, l.Len(), 0)
+	Nil(t, l.First())
+	Nil(t, l.Last())
+	Nil(t, l.Pop())
+}
+
+func TestCopy(t *testing.T) {
+	l1 := newFilledList(t)
+
+	l2 := l1.Copy()
+
+	Equal(t, l1.Len(), l2.Len())
+
+	n1 := l1.First()
+	n2 := l2.First()
+
+	for n1 != nil && n2 != nil {
+		Equal(t, n1.Value, n2.Value)
+
+		n1 = n1.Next()
+		n2 = n2.Next()
+	}
+
+	Nil(t, n1)
+	Nil(t, n2)
+}
+
+func TestFind(t *testing.T) {
+	l := New()
+
+	for _, vi := range v {
+		f, ok := l.IndexOf(vi)
+		Equal(t, f, -1)
+		Equal(t, ok, false)
+
+		ok = l.Contains(vi)
+		Equal(t, ok, false)
+	}
+
+	fillList(t, l)
+
+	for i, vi := range v {
+		f, ok := l.IndexOf(vi)
+		Equal(t, f, i)
+		Equal(t, ok, true)
+
+		ok = l.Contains(vi)
+		Equal(t, ok, true)
+	}
+
+	l.Clear()
+
+	f, ok := l.IndexOf(0)
+	Equal(t, f, -1)
+	Equal(t, ok, false)
+
+	f, ok = l.LastIndexOf(0)
+	Equal(t, f, -1)
+	Equal(t, ok, false)
+
+	for i := 0; i < 4; i++ {
+		l.Push(0)
+
+		f, ok = l.IndexOf(0)
+		Equal(t, f, 0)
+		Equal(t, ok, true)
+
+		f, ok = l.LastIndexOf(0)
+		Equal(t, f, i)
+		Equal(t, ok, true)
+	}
+
+	// not found in nonempty list
+	f, ok = l.IndexOf(100)
+	Equal(t, f, -1)
+	Equal(t, ok, false)
+
+	f, ok = l.LastIndexOf(100)
+	Equal(t, f, -1)
+	Equal(t, ok, false)
+}
+
+func TestGetSet(t *testing.T) {
+	l := New()
+
+	for i := range v {
+		n, err := l.Get(i)
+
+		Nil(t, n)
+		NotNil(t, err)
+
+		err = l.Set(i, i+10)
+
+		NotNil(t, err)
+
+		n, err = l.Get(i)
+
+		Nil(t, n)
+		NotNil(t, err)
+	}
+
+	fillList(t, l)
+
+	for i, vi := range v {
+		n, err := l.Get(i)
+
+		Equal(t, n.Value, vi)
+		Nil(t, err)
+
+		err = l.Set(i, i+10)
+
+		Nil(t, err)
+
+		n, err = l.Get(i)
+
+		Equal(t, n.Value, i+10)
+		Nil(t, err)
+	}
+}
+
+func TestAddLists(t *testing.T) {
+	l1 := New()
+	l1.Push(3)
+	l1.Push(4)
+
+	l2 := New()
+	l2.Push(5)
+	l2.Push(6)
+
+	l3 := New()
+	l3.Push(2)
+	l3.Push(1)
+
+	l1.PushList(l2)
+	Equal(t, l1.ToArray(), []interface{}{3, 4, 5, 6})
+
+	l1.UnshiftList(l3)
+	Equal(t, l1.ToArray(), []interface{}{1, 2, 3, 4, 5, 6})
+}
+
+func TestMoves(t *testing.T) {
+	l := newFilledList(t)
+
+	n := l.First()
+	p := l.Last()
+	l.MoveAfter(n, p)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, "b", 1})
+	Equal(t, l.Len(), vLen)
+
+	n = l.Last()
+	p = l.First()
+	l.MoveAfter(n, p)
+	Equal(t, l.ToArray(), []interface{}{"a", 1, 2, "b"})
+	Equal(t, l.Len(), vLen)
+
+	n, _ = l.Get(1)
+	p, _ = l.Get(2)
+	l.MoveAfter(n, p)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, 1, "b"})
+	Equal(t, l.Len(), vLen)
+
+	l.Push(0)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, 1, "b", 0})
+	Equal(t, l.Len(), vLen+1)
+
+	l = newFilledList(t)
+
+	n = l.First()
+	p = l.Last()
+	l.MoveBefore(n, p)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, 1, "b"})
+	Equal(t, l.Len(), vLen)
+
+	n = l.Last()
+	p = l.First()
+	l.MoveBefore(n, p)
+	Equal(t, l.ToArray(), []interface{}{"b", "a", 2, 1})
+	Equal(t, l.Len(), vLen)
+
+	n, _ = l.Get(1)
+	p, _ = l.Get(2)
+	l.MoveBefore(n, p)
+	Equal(t, l.ToArray(), []interface{}{"b", "a", 2, 1})
+	Equal(t, l.Len(), vLen)
+
+	l.Push(0)
+	Equal(t, l.ToArray(), []interface{}{"b", "a", 2, 1, 0})
+	Equal(t, l.Len(), vLen+1)
+
+	l = newFilledList(t)
+
+	n = l.First()
+	l.MoveToBack(n)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, "b", 1})
+	Equal(t, l.Len(), vLen)
+
+	n = l.Last()
+	l.MoveToBack(n)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, "b", 1})
+	Equal(t, l.Len(), vLen)
+
+	n, _ = l.Get(2)
+	l.MoveToBack(n)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, 1, "b"})
+	Equal(t, l.Len(), vLen)
+
+	l.Push(0)
+	Equal(t, l.ToArray(), []interface{}{"a", 2, 1, "b", 0})
+	Equal(t, l.Len(), vLen+1)
+
+	l = newFilledList(t)
+
+	n = l.First()
+	l.MoveToFront(n)
+	Equal(t, l.ToArray(), []interface{}{1, "a", 2, "b"})
+	Equal(t, l.Len(), vLen)
+
+	n = l.Last()
+	l.MoveToFront(n)
+	Equal(t, l.ToArray(), []interface{}{"b", 1, "a", 2})
+	Equal(t, l.Len(), vLen)
+
+	n, _ = l.Get(2)
+	l.MoveToFront(n)
+	Equal(t, l.ToArray(), []interface{}{"a", "b", 1, 2})
+	Equal(t, l.Len(), vLen)
+
+	l.Push(0)
+	Equal(t, l.ToArray(), []interface{}{"a", "b", 1, 2, 0})
+	Equal(t, l.Len(), vLen+1)
+
+	// do not allow moving nodes from another list
+	l1 := newFilledList(t)
+	l2 := newFilledList(t)
+
+	n1, _ := l1.Get(1)
+	n2, _ := l2.Get(2)
+
+	l1.MoveAfter(n1, n2)
+	Equal(t, l1.ToArray(), v)
+	Equal(t, l2.ToArray(), v)
+
+	l1.MoveAfter(n2, n1)
+	Equal(t, l1.ToArray(), v)
+	Equal(t, l2.ToArray(), v)
+
+	l1.MoveBefore(n1, n2)
+	Equal(t, l1.ToArray(), v)
+	Equal(t, l2.ToArray(), v)
+
+	l1.MoveBefore(n2, n1)
+	Equal(t, l1.ToArray(), v)
+	Equal(t, l2.ToArray(), v)
+
+	l1.MoveToBack(n2)
+	Equal(t, l1.ToArray(), v)
+	Equal(t, l2.ToArray(), v)
+
+	l1.MoveToFront(n2)
+	Equal(t, l1.ToArray(), v)
+	Equal(t, l2.ToArray(), v)
+}
+
+func TestFunc(t *testing.T) {
+	l := newFilledList(t)
+
+	Equal(t, v[1], l.GetFunc(func(n *Node) bool {
+		return n.Value == "a"
+	}).Value)
+	Nil(t, l.GetFunc(func(n *Node) bool {
+		return n.Value == "z"
+	}))
+
+	l.SetFunc(func(n *Node) bool {
+		return n.Value == 2
+	}, 3)
+	Equal(t, l.ToArray(), []interface{}{1, "a", 3, "b"})
+	l.SetFunc(func(n *Node) bool {
+		return n.Value == "z"
+	}, 4)
+	Equal(t, l.ToArray(), []interface{}{1, "a", 3, "b"})
+}
+
+*/
